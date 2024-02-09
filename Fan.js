bc7bde2be858888c4d68d6b513b213018548ccbc
Navdeep Pathan <151203532+navdeeppathan@users.noreply.github.com>
2024-02-09 21:10:02 +0530

    Add files via upload

---
+++ Fan.js
@@ -0,0 +1,16 @@
+let a=document.getElementById("img");
+function myFanOn(){
+    a.style.animationDuration=3+"s";
+}
+function myFanOff(){
+    a.style.animationDuration=0+"s";
+}
+function myFan1(){
+    a.style.animationDuration=1+"s";
+}
+function myFan2(){
+    a.style.animationDuration=0.5+"s";
+}
+function myFan3(){
+    a.style.animationDuration=0.1+"s";
+}
